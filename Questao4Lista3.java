012217fac2b3d0309dce278e19937f8cb14d7dcf
mauricio807 <38573479+mauricio807@users.noreply.github.com>
2018-05-23 17:50:24 -0300

    Add files via upload

---
+++ Questao4Lista3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mauri
+ */
+public class Questao4Lista3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 1000; i < 1999; i++) {
+            if (i %11==5){
+                System.out.println("este Número dvidido por 11 tem o resto 5 ->" +i+"");
+                System.out.println("");
+            }
+        }
+    }
+    
+}
